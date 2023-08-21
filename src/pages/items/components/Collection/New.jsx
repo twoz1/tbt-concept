224ab3693720040c78371200ea4645e306eb907e
User -2023NJGNF\Administrator <>
2023-08-21 18:40:40 +0900

    0821

--- src/pages/items/components/Collection/New.jsx
+++ src/pages/items/components/Collection/New.jsx
@@ -15,7 +15,7 @@ const New = () => {
 
     function clickBackBtn(e) {
         what_new_idx.current--;
-        what_new_list.current.style.left = `${-what_new_idx.current * 25}%`;
+        what_new_list.current.style.left = `${-what_new_idx.current * 265}px`;
 
         btn_next.current.classList.remove('nonVisible');
         if (what_new_idx.current <= 0) {
@@ -26,7 +26,12 @@ const New = () => {
 
     function clickNext(e) {
         what_new_idx.current++;
-        what_new_list.current.style.left = `${-what_new_idx.current * 26}%`;
+        
+        if (what_new_idx.current === 1) {
+            what_new_list.current.style.left = `${-what_new_idx.current * 225}px`;
+        } else {
+            what_new_list.current.style.left = `${-what_new_idx.current * 245}px`;
+        }
 
         btn_pre.current.classList.remove('nonVisible');
         if (what_new_idx.current >= 4) {
@@ -35,17 +40,30 @@ const New = () => {
         toggleItemWidth();
     }
     function toggleItemWidth() {
-        const evenItems = what_new_list.current.querySelectorAll('.what_new_list li:nth-child(even) img');
-        const oddItems = what_new_list.current.querySelectorAll('.what_new_list li:nth-child(odd) img');
+        const evenItems = what_new_list.current.querySelectorAll('.what_new_list li:nth-child(even)');
+        const oddItems = what_new_list.current.querySelectorAll('.what_new_list li:nth-child(odd)');
 
         evenItems.forEach((item) => {
-            item.style.width = '273px'; // odd 아이템의 width로 설정
+            item.style.width = '255px'; // odd 아이템의 width로 설정
         });
 
         oddItems.forEach((item) => {
-            item.style.width = '230px'; // even 아이템의 width로 설정
+            item.style.width = '215px'; // even 아이템의 width로 설정
         });
     }
+
+    // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+    const [newList2, setNewList2] = useState(newList.slice(0, 8));
+
+    // const handlePrevClick = () => {
+    //     setNewList2(prevList => prevList.slice(0, prevList.length - 1));
+    // };
+
+    // const handleNextClick = () => {
+    //     setNewList2(prevList => prevList.slice(1)); // Remove the first item
+    // };
+
     return (
         <div className="what_new">
             <div className="what_new_title cf">
@@ -59,22 +77,25 @@ const New = () => {
                     </h2>
                 </Link>
             </div>
+
             <button className="btn_pre nonVisible" ref={btn_pre} onClick={clickBackBtn}>
                 <FontAwesomeIcon icon={faChevronLeft} />
             </button>
             <div className="what_new_item">
-                <ul className="what_new_list" ref={what_new_list}>
-                    {newList.slice(0, 8).map((item) => (
+                <ul className="what_new_list">
+                    {newList2.map((item) => (
                         <li>
                             <Link to={`/detail/${item.name}`} key={item.name}>
                                 <img src={item.imageFront} alt="상품" />
                                 <img src={item.imageSide} alt="상품" />
+                            </Link>
                                 <div className="item_name">
                                     <span>{item.name}</span>
                                 </div>
                                 <div className="item_price">
                                     <span>{item.price.toLocaleString()}원</span>
                                 </div>
+                            <Link to={`/detail/${item.name}`} key={item.name}>
                                 <div className="shop_this">
                                     <a>{item.shopThis} &#62;</a>
                                 </div>
@@ -86,6 +107,7 @@ const New = () => {
             <button className="btn_next" ref={btn_next} onClick={clickNext}>
                 <FontAwesomeIcon icon={faChevronRight} />
             </button>
+
         </div>
     );
 };
