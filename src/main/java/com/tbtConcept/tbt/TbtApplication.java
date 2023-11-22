dc91f7dc703d6f0b4a58d8d6895c96de3310319c
HAZE\SAMSUNG <hazedensays@gmail.com>
2023-11-22 17:54:47 +0900

    2차 프로젝트 삭제 후 3차 프로젝트 생성

---
+++ src/main/java/com/tbtConcept/tbt/TbtApplication.java
@@ -0,0 +1,14 @@
+package com.tbtConcept.tbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbtApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/tbtConcept/tbt/TbtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbtConcept.tbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
