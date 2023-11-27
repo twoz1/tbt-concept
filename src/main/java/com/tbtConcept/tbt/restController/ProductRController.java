b0632586823583e65653239833c50afeb1da7f69
DESKTOP-GD3SP7B\User <hana_smile@naver.com>
2023-11-27 20:07:05 +0900

    1127 react와 db연동 테스트

---
+++ src/main/java/com/tbtConcept/tbt/restController/ProductRController.java
@@ -0,0 +1,44 @@
+package com.tbtConcept.tbt.restController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.tbtConcept.tbt.entity.Product;
+import com.tbtConcept.tbt.service.ProductService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@AllArgsConstructor
+@RequestMapping(value = "/product")
+@RestController
+public class ProductRController {
+
+	ProductService prodService;
+
+	@GetMapping("/pListDesc")
+	public List<Product> getPListDesc(Model model) {
+		return prodService.findAllDesc();
+	}
+	
+
+}
