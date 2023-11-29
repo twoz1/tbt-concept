02ba51c02b8cb91cd17c6fb7f330d8f781eba7b1
DESKTOP-GD3SP7B\User <hana_smile@naver.com>
2023-11-29 11:36:49 +0900

    20231129

--- src/main/java/com/tbtConcept/tbt/restController/UserRController.java
+++ src/main/java/com/tbtConcept/tbt/restController/UserRController.java
@@ -1,4 +1,4 @@
-package com.tbtConcept.tbt.controller;
+package com.tbtConcept.tbt.restController;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,15 +7,14 @@ import javax.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.tbtConcept.tbt.entity.Product;
 import com.tbtConcept.tbt.entity.User;
 import com.tbtConcept.tbt.service.UserService;
 
@@ -24,10 +23,10 @@ import lombok.extern.log4j.Log4j2;
 
 
 @AllArgsConstructor
-@RequestMapping(value = "/master/user")
-@Controller
+@RequestMapping(value = "/user")
+@RestController
 @Log4j2
-public class UserController {
+public class UserRController {
 	UserService userService;
 	PasswordEncoder passwordEncoder;
 
@@ -54,7 +53,7 @@ public class UserController {
 		
 	} 
 	
-	@PostMapping(value="/userJoin")
+	@PostMapping(value="/uJoin")
 	public String postUserJoin(HttpServletRequest request, Model model , User entity)  {
 		String uri ="redirect:userList";
 		entity.setUser_pw(passwordEncoder.encode(entity.getUser_pw()));
