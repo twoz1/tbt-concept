ac99dfd150a197351c58ee12bcf2dd91482be735
DESKTOP-TAETHK5\LMY <wlsgnl502@gmail.com>
2023-12-15 17:59:06 +0900

    20231215 페이지네이션 구현완료

--- src/main/java/com/tbtConcept/tbt/repository/OrderListRepository.java
+++ src/main/java/com/tbtConcept/tbt/repository/OrderListRepository.java
@@ -13,9 +13,15 @@ import com.tbtConcept.tbt.entity.OrderList;
 public interface OrderListRepository extends JpaRepository<OrderList, String> {
 //    @Query("SELECT o FROM OrderList o order by o.order_date desc")
 //    List<OrderList> findAllDesc();
-    @Query("SELECT o FROM OrderList o order by o.order_date desc")
-    Page<OrderList> findAllDesc(Pageable pageable);
+    @Query("SELECT o FROM OrderList o WHERE (:keyword = '' OR :searchType = 'all' OR"+
+    		"(:searchType = 'order_date' AND o.order_date LIKE %:keyword%) OR " +
+    		"(:searchType = 'user_id' AND o.user_id LIKE %:keyword%)) " + 
+    		"ORDER BY o.order_date desc")
+    Page<OrderList> findAllDesc(Pageable pageable,  @Param("searchType") String searchType, @Param("keyword") String keyword);
     
+//    @Query("SELECT o FROM OrderList o order by o.order_date desc")
+//    Page<OrderList> findAllDesc(Pageable pageable);
+//    
     
     @Query(nativeQuery = true, value = "SELECT * FROM order_list WHERE user_id = :user_id ORDER BY order_date DESC")
     List<OrderList> userOrderList(@Param("user_id") String id);
