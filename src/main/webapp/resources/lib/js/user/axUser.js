753a3d61b607ed16583c6424ecac50bf9ed749f6
DESKTOP-GD3SP7B\User <hana_smile@naver.com>
2023-12-18 10:04:17 +0900

    1218 wish delete 적용

--- src/main/webapp/resources/lib/js/user/axUser.js
+++ src/main/webapp/resources/lib/js/user/axUser.js
@@ -21,3 +21,28 @@ function axUserDelete(id) {
 		alert("취소되었습니다.");
 	}
 }
+function axWishDelete(user_id, product_id) {
+
+	let url = `/master/user/wishDelete/${user_id}/${product_id}`;
+
+	if (confirm("삭제하시겠습니까?")) {
+		axios.delete(url)
+			.then(response => {
+				if (response.status === 200) {
+					alert("삭제되었습니다.");
+					location.reload();
+				} else {
+					alert("[관심상품 삭제 오류] 삭제 실패");
+				}
+			})
+			.catch(err => {
+				if (err.response && err.response.status === 502) {
+					alert("[관심상품 삭제 오류] " + err.response.data);
+				} else {
+					alert("[관심상품 시스템 오류] " + err.message);
+				}
+			});
+	} else {
+		alert("취소되었습니다.");
+	}
+}
